6dd777db4bf70a59c6d7b032c465ec46f4621bfc
Dean Nellis <nellisdean@gmail.com>
2018-05-07 20:46:01 +0000

    Added onClick event

--- src/components/Die.js
+++ src/components/Die.js
@@ -92,8 +92,7 @@ export default class DieFour extends React.Component {
                     onMouseDown={this.onMouseDown}
                     onMouseUp={this.onMouseUp}
                     onMouseOut={this.onMouseOut}
-                    onTouchStart={this.onMouseDown}
-                    onTouchEnd={this.onMouseUp}
+                    onClick={this.onMouseUp}
                 />
                 <p 
                     className={`die__result--v2 ${typeof this.time != 'undefined' && 'die__result--hide'}`} //&& 'die__result--hide'
